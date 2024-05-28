1ae16e0587529d29c1f0b0412c77f4805b789392
rodertd2000 <zdabaghov@mail.ru>
2024-05-28 17:56:10 +0300

    shuffle answers

--- src/components/Answers/index.tsx
+++ src/components/Answers/index.tsx
@@ -1,16 +1,22 @@
 import { useAppSelector } from "../../redux/store";
+import { shuffleArray } from "../../utils/array";
 import { MultipleAnswers } from "./Multiple";
 import { SingleAnswers } from "./Single";
 
 export const Answers = () => {
   const { currentQuestion } = useAppSelector((state) => state.questionsSlice);
 
+  const anwers = shuffleArray([
+    ...(currentQuestion?.incorrect_answers as string[]),
+    currentQuestion?.correct_answer || "",
+  ]);
+
   return (
     <div>
       {currentQuestion?.type === "multiple" ? (
-        <MultipleAnswers options={currentQuestion?.incorrect_answers} />
+        <MultipleAnswers options={anwers} />
       ) : (
-        <SingleAnswers options={currentQuestion?.incorrect_answers || []} />
+        <SingleAnswers options={anwers} />
       )}
     </div>
   );
