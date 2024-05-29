58100d5dbdd1b4ba86fcf7739af4cdbc7026cc5d
rodertd2000 <zdabaghov@mail.ru>
2024-05-29 21:34:42 +0300

    clean up

--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,5 +1,9 @@
 import "@testing-library/jest-dom";
+import { http, delay, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 import { vi } from "vitest";
+import { BASE_URL } from "./constants";
+import { mockQuestions } from "./tests/mocks/questions";
 
 Object.defineProperty(window, "matchMedia", {
   writable: true,
@@ -14,3 +18,22 @@ Object.defineProperty(window, "matchMedia", {
     dispatchEvent: vi.fn(),
   })),
 });
+
+export const handlers = [
+  http.get(`${BASE_URL}?amount=10`, async () => {
+    await delay(100);
+
+    return HttpResponse.json(mockQuestions);
+  }),
+];
+
+const server = setupServer(...handlers);
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers());
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
--- src/tests/Questions/Questions.test.tsx
+++ src/tests/Questions/Questions.test.tsx
@@ -1,29 +1,7 @@
-import { http, HttpResponse, delay } from "msw";
-import { setupServer } from "msw/node";
+import { delay } from "msw";
 import { fireEvent, screen } from "@testing-library/react";
 import { renderWithProviders } from "../utils/test-utils";
 import { Questions } from "../../components/Questions";
-import { mockQuestions } from "../mocks/questions";
-import { BASE_URL } from "../../constants";
-
-export const handlers = [
-  http.get(`${BASE_URL}?amount=10`, async () => {
-    await delay(100);
-
-    return HttpResponse.json(mockQuestions);
-  }),
-];
-
-const server = setupServer(...handlers);
-
-// Enable API mocking before tests.
-beforeAll(() => server.listen());
-
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers());
-
-// Disable API mocking after the tests are done.
-afterAll(() => server.close());
 
 test("rendering questions, select and move to next", async () => {
   renderWithProviders(<Questions />);
