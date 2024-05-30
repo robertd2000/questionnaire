0805785f2c71e8876a45450cca1e368ce22492f7
rodertd2000 <zdabaghov@mail.ru>
2024-05-30 09:29:02 +0300

    test check false

--- src/tests/functions/check.test.ts
+++ src/tests/functions/check.test.ts
@@ -3,12 +3,15 @@ import { checkIsCorrect } from "../../utils/check";
 
 test("Test check util (2 string parameters)", () => {
   expect(checkIsCorrect("True", "True")).toBe(true);
+  expect(checkIsCorrect("True", "false")).toBe(false);
 });
 
 test("Test check util (string parameter and array parameter)", () => {
   expect(checkIsCorrect(["True"], "True")).toBe(true);
+  expect(checkIsCorrect(["True"], "false")).toBe(false);
 });
 
 test("Test check util (string parameter and array parameter)", () => {
   expect(checkIsCorrect("True", ["True"])).toBe(true);
+  expect(checkIsCorrect("True", ["False"])).toBe(false);
 });
