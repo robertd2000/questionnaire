319354db5df48cdcd1cba0916da6aacf8673b434
rodertd2000 <zdabaghov@mail.ru>
2024-05-30 11:40:38 +0300

    compaire

--- src/redux/questions/api/asyncActions.ts
+++ src/redux/questions/api/asyncActions.ts
@@ -1,14 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { getQuestions } from "../../../api/questions.api";
 
-export const fetchQuestions = createAsyncThunk(
-  "userAccount/logIn",
-  async () => {
-    try {
-      const data = await getQuestions();
-      return data;
-    } catch (error) {
-      return Promise.reject(error);
-    }
+export const fetchQuestions = createAsyncThunk("questions/get", async () => {
+  try {
+    const data = await getQuestions();
+    return data;
+  } catch (error) {
+    return Promise.reject(error);
   }
-);
+});
